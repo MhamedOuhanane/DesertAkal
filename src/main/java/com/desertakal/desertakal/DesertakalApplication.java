ba977c076327e6d668b0de668d38617b9c977522
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2025-12-06 02:08:21 +0100

    fixes FRD-31 Créer le projet Spring Boot via Spring Initializr

---
+++ src/main/java/com/desertakal/desertakal/DesertakalApplication.java
@@ -0,0 +1,13 @@
+package com.desertakal.desertakal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesertakalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesertakalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desertakal/desertakal/DesertakalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desertakal.desertakal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesertakalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
