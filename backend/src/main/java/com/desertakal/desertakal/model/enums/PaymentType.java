ae52321f8c83f644f121942627aedd9d57c6e70c
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2025-12-11 15:26:41 +0100

    FRD-75 #done: Cree énumération PaymentType pour le type de paiement

---
+++ backend/src/main/java/com/desertakal/desertakal/model/enums/PaymentType.java
@@ -0,0 +1,15 @@
+package com.desertakal.desertakal.model.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum PaymentType {
+    PAYMENT("Make a payment"),
+    REFUND("Refunded amount");
+
+    private final String desc;
+
+    PaymentType(String desc) {
+        this.desc = desc;
+    }
+}
