aa69714708144c3f4a6fa23cff33a4209a6311ab
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2025-12-11 15:14:13 +0100

    FRD-74 #done: Cree énumération PaymentStatus pour le statut des paiements

---
+++ backend/src/main/java/com/desertakal/desertakal/model/enums/PaymentStatus.java
@@ -0,0 +1,19 @@
+package com.desertakal.desertakal.model.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum PaymentStatus {
+    PENDING("Payment is pending"),
+    COMPLETED("Payment completed successfully"),
+    FAILED("Payment failed"),
+    CANCELED("Payment was canceled"),
+    REFUNDED("Payment fully refunded"),
+    REFUNDED_PARTIAL("Payment partially refunded");
+
+    private final String desc;
+
+    PaymentStatus(String desc) {
+        this.desc = desc;
+    }
+}
