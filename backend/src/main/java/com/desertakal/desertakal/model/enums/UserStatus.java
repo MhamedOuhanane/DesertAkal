dcce7613a9004bca37c26292e77898b3cd59cc98
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2025-12-11 15:03:50 +0100

    FRD-72 #done: Cree énumération UserStatus pour le statut de chaque utilisateurs

--- backend/src/main/java/com/desertakal/desertakal/model/enums/UserStatus.java
+++ backend/src/main/java/com/desertakal/desertakal/model/enums/UserStatus.java
@@ -1,4 +1,16 @@
 package com.desertakal.desertakal.model.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum UserStatus {
+    ACTIVE("User is active"),
+    SUSPENDED("User is suspended"),
+    BANNED("User is banned");
+
+    private final String desc;
+
+    UserStatus(String desc) {
+        this.desc = desc;
+    }
 }
