2fa8bed1959bfc74600d7b60eb5f265734077efe
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2025-12-11 22:28:14 +0100

    FRD-84 #done: Cree l'entité Tourist avec ses attribues et ses méthode

---
+++ backend/src/main/java/com/desertakal/desertakal/model/entity/Tourist.java
@@ -0,0 +1,28 @@
+package com.desertakal.desertakal.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tourists")
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+public class Tourist extends User {
+    @Column(name = "avatar_url", nullable = false)
+    private String avatarUrl;
+
+    @Column(nullable = false)
+    private String nationality;
+
+    @Column(nullable = false)
+    private String language;
+}
