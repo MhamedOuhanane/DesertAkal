a95a1662a070cc3dece2d8d9107fcb25395477de
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2025-12-11 22:35:14 +0100

    FRD-85 #done: Cree l'entité Image avec ses attribues et ses méthode

---
+++ backend/src/main/java/com/desertakal/desertakal/model/entity/Image.java
@@ -0,0 +1,45 @@
+package com.desertakal.desertakal.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "images")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+
+    @EqualsAndHashCode.Include
+    @Column(columnDefinition = "uuid", updatable = false, nullable = false, unique = true)
+    protected UUID uuid;
+
+    @Column(nullable = false)
+    private String image;
+
+    @Column(name = "is_cover", nullable = false)
+    protected Boolean is_cover;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false)
+    protected LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist(){
+        if (uuid == null)
+            uuid = UUID.randomUUID();
+    }
+}
