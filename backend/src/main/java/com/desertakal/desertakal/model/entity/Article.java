56e396164120465fb84172ad66363bdaeb444678
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2025-12-12 12:13:42 +0100

    FRD-93 #done: Cree l'entité Article avec ses attribues, ses méthode et ses relations

---
+++ backend/src/main/java/com/desertakal/desertakal/model/entity/Article.java
@@ -0,0 +1,62 @@
+package com.desertakal.desertakal.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "articles")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+
+    @EqualsAndHashCode.Include
+    @Column(columnDefinition = "uuid", updatable = false, nullable = false, unique = true)
+    protected UUID uuid;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(name = "cover_image", nullable = false)
+    private String coverImage;
+
+    @Column(name = "reaction_count", nullable = false)
+    private Integer reactionCount;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false)
+    protected LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    protected LocalDateTime updatedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "city_id")
+    private City city;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @PrePersist
+    public void prePersist(){
+        if (uuid == null)
+            uuid = UUID.randomUUID();
+    }
+
+    public void incrementReactionCount() {
+        reactionCount = (reactionCount == null ? 0 : reactionCount) + 1;
+    }
+}
