e25693d8294ef8c799711bd984c836ea4047087f
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2025-12-12 21:30:26 +0100

    FRD-98 #done: Ajouter un classe dto de role pour update

---
+++ backend/src/main/java/com/desertakal/desertakal/model/dto/permission/PermissionUpdateDTO.java
@@ -0,0 +1,17 @@
+package com.desertakal.desertakal.model.dto.permission;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PermissionUpdateDTO {
+    private UUID uuid;
+    private String name;
+}
