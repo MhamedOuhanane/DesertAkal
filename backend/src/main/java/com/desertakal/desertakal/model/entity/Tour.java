4538cf9a315a9b18ac321a74269298bd3fff7520
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2025-12-12 09:40:11 +0100

    FRD-87 #done: Cree l'entité Tour avec ses attribues, ses méthode et ses relations

---
+++ backend/src/main/java/com/desertakal/desertakal/model/entity/Tour.java
@@ -0,0 +1,56 @@
+package com.desertakal.desertakal.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tours")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Tour {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+
+    @EqualsAndHashCode.Include
+    @Column(columnDefinition = "uuid", updatable = false, nullable = false, unique = true)
+    protected UUID uuid;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private Integer durationDays;
+
+    @Column(nullable = false)
+    private String image;
+
+    @Column(nullable = false, precision = 3, scale = 2)
+    protected Boolean rating;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false)
+    protected LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    protected LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist(){
+        if (uuid == null)
+            uuid = UUID.randomUUID();
+    }
+}
