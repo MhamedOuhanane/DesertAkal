a4bc51e9bbf5b2b1e95326c28c80fc2f7f0c17d7
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2025-12-20 22:21:44 +0100

    FRD-140 #done: Ajouter la validation des données pour CityTour DTOs

--- backend/src/main/java/com/desertakal/desertakal/model/dto/cityTour/CityTourCreateDTO.java
+++ backend/src/main/java/com/desertakal/desertakal/model/dto/cityTour/CityTourCreateDTO.java
@@ -1,5 +1,7 @@
 package com.desertakal.desertakal.model.dto.cityTour;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.util.UUID;
@@ -10,7 +12,13 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CityTourCreateDTO {
+    @NotNull(message = "Order index is required")
+    @Min(value = 0, message = "Order index must be greater or equal to 0")
     private Integer orderIndex;
+
+    @NotNull(message = "City UUID is required")
     private UUID cityUuid;
+
+    @NotNull(message = "Tour UUID is required")
     private UUID tourUuid;
 }
