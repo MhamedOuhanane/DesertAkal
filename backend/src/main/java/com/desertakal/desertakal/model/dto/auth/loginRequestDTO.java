23f9bc88822635b8e0faabbc673225ab5c303998
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2025-12-20 21:38:53 +0100

    FRD-133 #done: Ajouter la validation des données pour Login DTO

--- backend/src/main/java/com/desertakal/desertakal/model/dto/auth/loginRequestDTO.java
+++ backend/src/main/java/com/desertakal/desertakal/model/dto/auth/loginRequestDTO.java
@@ -1,6 +1,8 @@
 package com.desertakal.desertakal.model.dto.auth;
 
 import com.desertakal.desertakal.model.enums.OauthProvider;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -10,7 +12,12 @@ import lombok.*;
 @AllArgsConstructor
 @EqualsAndHashCode
 public class loginRequestDTO {
+    @NotBlank(message = "Username is required")
+    @Size(min = 4, max = 50, message = "Username must be between 4 and 50 characters")
     private String username;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
     private String password;
 
     private OauthProvider provider;
