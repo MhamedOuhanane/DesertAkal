277cf6bf62ccbe3195fba00e6b887ef7498facec
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2025-12-20 13:43:17 +0100

    FRD-120 #done: Créer le mapping LanguageMapper

---
+++ backend/src/main/java/com/desertakal/desertakal/model/mapper/LanguageMapper.java
@@ -0,0 +1,22 @@
+package com.desertakal.desertakal.model.mapper;
+
+import com.desertakal.desertakal.model.dto.language.LanguageCreateDTO;
+import com.desertakal.desertakal.model.dto.language.LanguageDTO;
+import com.desertakal.desertakal.model.entity.Language;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface LanguageMapper {
+
+    LanguageDTO toDto(Language permission);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "uuid", ignore = true)
+    @Mapping(target = "guides", ignore = true)
+    Language toEntity(LanguageCreateDTO dto);
+
+    List<LanguageDTO> toDtos(List<Language> permissions);
+}
