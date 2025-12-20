bb832a7445543d922ebe5d4997a67c9bea5c81c6
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2025-12-20 22:57:44 +0100

    FRD-146 #done: Ajouter la validation des données pour Comment DTOs

--- backend/src/main/java/com/desertakal/desertakal/model/dto/comment/CommentCreateDTO.java
+++ backend/src/main/java/com/desertakal/desertakal/model/dto/comment/CommentCreateDTO.java
@@ -1,5 +1,7 @@
 package com.desertakal.desertakal.model.dto.comment;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.util.UUID;
@@ -10,5 +12,7 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CommentCreateDTO {
+    @NotBlank(message = "Content is required")
+    @Size(min = 1, max = 1000, message = "Content must be between 1 and 1000 characters")
     private String content;
 }
