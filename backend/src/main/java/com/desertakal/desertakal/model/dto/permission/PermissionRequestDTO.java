b27423a39124eba7ea26f2deba0fe0f5e789270f
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2025-12-20 21:45:33 +0100

    FRD-134 #done: Ajouter la validation des données pour Permission DTOs

--- backend/src/main/java/com/desertakal/desertakal/model/dto/permission/PermissionRequestDTO.java
+++ backend/src/main/java/com/desertakal/desertakal/model/dto/permission/PermissionRequestDTO.java
@@ -1,5 +1,7 @@
 package com.desertakal.desertakal.model.dto.permission;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Setter
@@ -9,5 +11,7 @@ import lombok.*;
 @AllArgsConstructor
 @EqualsAndHashCode
 public class PermissionRequestDTO {
+    @NotBlank(message = "Permission name is required")
+    @Size(min = 3, max = 50, message = "Permission name must be between 3 and 50 characters")
     private String name;
 }
