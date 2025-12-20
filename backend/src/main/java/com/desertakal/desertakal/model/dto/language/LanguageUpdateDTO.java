3c067c4a691b81f30acdb515a72fbfa36677d0f1
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2025-12-20 22:07:26 +0100

    FRD-137 #done: Ajouter la validation des données pour Language DTOs

--- backend/src/main/java/com/desertakal/desertakal/model/dto/language/LanguageUpdateDTO.java
+++ backend/src/main/java/com/desertakal/desertakal/model/dto/language/LanguageUpdateDTO.java
@@ -1,5 +1,8 @@
 package com.desertakal.desertakal.model.dto.language;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -9,6 +12,9 @@ import lombok.*;
 @AllArgsConstructor
 @EqualsAndHashCode
 public class LanguageUpdateDTO {
+    @Size(min = 4, max = 50, message = "Language name must be between 4 and 50 characters")
     private String name;
+
+    @Pattern(regexp = "^[a-z]{2,3}$", message = "Language code must be 2 or 3 lowercase letters")
     private String code;
 }
