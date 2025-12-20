d0ab4db5cf9af3f2752584a851dd18e5fd4d92c8
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2025-12-20 20:54:36 +0100

    FRD-132 #done: Créer le mapping ReactionMapper

--- backend/src/main/java/com/desertakal/desertakal/model/dto/reaction/ReactionCreateDTO.java
+++ backend/src/main/java/com/desertakal/desertakal/model/dto/reaction/ReactionCreateDTO.java
@@ -3,15 +3,11 @@ package com.desertakal.desertakal.model.dto.reaction;
 import com.desertakal.desertakal.model.enums.ReactionEnum;
 import lombok.*;
 
-import java.util.UUID;
-
 @Getter
 @Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class CreateReactionDTO {
+public class ReactionCreateDTO {
     private ReactionEnum reaction;
-    private UUID articleUuid;
-    private UUID userUuid;
 }
---
+++ backend/src/main/java/com/desertakal/desertakal/model/mapper/ReactionMapper.java
@@ -0,0 +1,33 @@
+package com.desertakal.desertakal.model.mapper;
+
+import com.desertakal.desertakal.model.dto.article.ArticleCreateDTO;
+import com.desertakal.desertakal.model.dto.article.ArticleDTO;
+import com.desertakal.desertakal.model.dto.article.ArticleUpdateDTO;
+import com.desertakal.desertakal.model.dto.reaction.ReactionCreateDTO;
+import com.desertakal.desertakal.model.dto.reaction.ReactionDTO;
+import com.desertakal.desertakal.model.entity.Article;
+import com.desertakal.desertakal.model.entity.Reaction;
+import org.mapstruct.*;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface ReactionMapper {
+
+    @Mapping(source = "user.uuid", target = "userUuid")
+    @Mapping(source = "article.uuid", target = "articleUuid")
+    @Mapping(source = "reaction.getDesc()", target = "emoji")
+    @Mapping(expression = "java(reservation.getUser().getFirstName() + \" \" + reservation.getUser().getLastName())", target = "userName")
+    @Mapping(source = "user.photo", target = "userPhoto")
+    ReactionDTO toDto(Reaction article);
+
+    @Mapping(target = "id" , ignore = true)
+    @Mapping(target = "uuid" , ignore = true)
+    @Mapping(target = "reaction" , ignore = true)
+    @Mapping(target = "createdAt" , ignore = true)
+    @Mapping(target = "user" , ignore = true)
+    @Mapping(target = "article" , ignore = true)
+    Reaction toEntity(ReactionCreateDTO dto);
+
+    List<ReactionDTO> toDtos(List<Reaction> articles);
+}
