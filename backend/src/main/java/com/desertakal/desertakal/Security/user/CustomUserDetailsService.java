eebf7d1678522838729f10ef6575e1d783c4f5e2
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2025-12-27 21:38:19 +0100

    FRD-164  #done: Cree la classe CustomUserDetailsService pour la gestion de CustomUserDetails

--- backend/src/main/java/com/desertakal/desertakal/Security/user/CustomUserDetailsService.java
+++ backend/src/main/java/com/desertakal/desertakal/Security/user/CustomUserDetailsService.java
@@ -1,15 +1,37 @@
 package com.desertakal.desertakal.Security.user;
 
+import com.desertakal.desertakal.exception.custom.ResourceNotFoundException;
+import com.desertakal.desertakal.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
+@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository repository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        return repository.findByEmailOrUsername(username)
+                .map(CustomUserDetails::new)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("User", "identifier", username)
+                );
+    }
+
+
+    @Cacheable(value = "users", key = "#uuid")
+    public UserDetails loadUserByUuid(String uuid) throws UsernameNotFoundException {
+        return repository.findByUuid(UUID.fromString(uuid))
+                .map(CustomUserDetails::new)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("User", "identifier", uuid)
+                );
     }
 }
