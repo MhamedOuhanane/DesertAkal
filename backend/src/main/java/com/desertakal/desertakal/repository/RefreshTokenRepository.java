1f0d6bdfd4f9d1ee773e6784710bcd2ed1cea097
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2025-12-28 16:49:11 +0100

    FRD-172 #done: Créer le dépôt RefreshTokenRepository

---
+++ backend/src/main/java/com/desertakal/desertakal/repository/RefreshTokenRepository.java
@@ -0,0 +1,38 @@
+package com.desertakal.desertakal.repository;
+
+import com.desertakal.desertakal.model.entity.RefreshToken;
+import com.desertakal.desertakal.model.entity.User;
+import org.jspecify.annotations.NonNull;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface RefreshTokenRepository extends JpaRepository<@NonNull RefreshToken, @NonNull UUID> {
+    Optional<RefreshToken> findByToken(String token);
+
+    List<RefreshToken> findByUserAndDeviceId(User user, String deviceId);
+
+    List<RefreshToken> findAllByFamilyId(String familyId);
+
+    Optional<RefreshToken> findAllByTokenAndIsRevoked(String token, boolean isRevoked);
+    Optional<RefreshToken> findAllByTokenAndIsUsed(String token, boolean isUsed);
+
+    List<RefreshToken> findAllByUserAndIsRevoked(User user, boolean isRevoked);
+    List<RefreshToken> findAllByUserAndIsUsed(User user, boolean isUsed);
+
+    List<RefreshToken> findAllByUserAndIsRevokedFalseAndIsUsedFalse(User user);
+    List<RefreshToken> findAllByUserAndIsReuseDetectedFalse(User user);
+
+    @Transactional
+    void deleteByUser(User user);
+    @Transactional
+    void deleteByFamilyId(String familyId);
+
+    @Transactional
+    void deleteByExpiresAtBefore(LocalDateTime dateTime);
+
+}
