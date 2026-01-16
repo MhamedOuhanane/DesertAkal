7d12e4b004b0a25f22a27d91eb470ec6a1c9e249
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2026-01-16 17:21:20 +0100

    FRD-202 #done: Réaliser la création d'un nouveau permission personnalisé

--- backend/src/main/java/com/desertakal/desertakal/controller/PermissionController.java
+++ backend/src/main/java/com/desertakal/desertakal/controller/PermissionController.java
@@ -1,22 +1,22 @@
 package com.desertakal.desertakal.controller;
 
 import com.desertakal.desertakal.model.dto.permission.PermissionDTO;
+import com.desertakal.desertakal.model.dto.permission.PermissionRequestDTO;
 import com.desertakal.desertakal.model.dto.responce.PaginationDTO;
 import com.desertakal.desertakal.model.dto.responce.StandardResponseDTO;
 import com.desertakal.desertakal.service.interfaces.PermissionService;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jspecify.annotations.NonNull;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 
@@ -57,5 +57,29 @@ public class PermissionController {
                 result.getTotalElements(), result.getTotalElements());
 
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<@NonNull StandardResponseDTO<@NonNull PermissionDTO>> create(
+            @NonNull @Valid @RequestBody PermissionRequestDTO dto,
+            @NonNull HttpServletRequest request
+    ) {
+        log.info("REST request to CREATE Permission: '{}' [Path: {}]",
+                dto.getName(), request.getServletPath());
+
+        var result = service.create(dto);
+
+        var response = StandardResponseDTO.<PermissionDTO>builder()
+                .timestamp(LocalDateTime.now())
+                .message("Permission '" + result.getName() + "' created successfully")
+                .status(201)
+                .data(result)
+                .path(request.getServletPath())
+                .build();
+
+        log.info("Permission successfully created with UUID: {} [Status: 201]", result.getUuid());
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
