26cc99d4cdc4be00e3faca25741d73be88e1bcbb
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2026-02-08 14:53:25 +0100

    FRD-270 #done: Installer et configurer Angular Material

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
@@ -9,5 +10,6 @@ export const appConfig: ApplicationConfig = {
         provideBrowserGlobalErrorListeners(),
         provideRouter(routes),
         provideClientHydration(withEventReplay()),
+        provideAnimations()
     ],
 };
