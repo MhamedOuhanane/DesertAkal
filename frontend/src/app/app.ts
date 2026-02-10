7e8469858b99c0e8cf590bf18aea1b65483d138c
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2026-02-10 21:24:40 +0100

    FRD-292 #done: Création du MainLayoutComponent (Header + RouterOutlet + Footer) pour les touristes et visiteurs.

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,26 +1,11 @@
 import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatIconModule } from '@angular/material/icon';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatChipsModule } from '@angular/material/chips';
 import { ThemeService } from './core/services/theme-service';
 
 @Component({
     selector: 'app-root',
     imports: [
-        MatButtonModule,
-        MatCardModule,
-        MatIconModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatSlideToggleModule,
-        MatCheckboxModule,
-        MatChipsModule,
+        RouterOutlet,
     ],
     templateUrl: './app.html',
     styleUrl: './app.scss',
