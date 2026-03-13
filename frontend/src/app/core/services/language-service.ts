0b99a469e7bedd532522b806272662a4879799d3
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2026-03-13 20:42:03 +0000

    FRD-372 #done: Configurer le service HTTP des langues

--- frontend/src/app/core/services/language-service.ts
+++ frontend/src/app/core/services/language-service.ts
@@ -1,21 +1,43 @@
-import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
-import { environment } from '../../../environments/environment.development';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 import { ApiResponse, Pagination } from '../models/response.models';
-import { Language, LanguageFilters } from '../models/language.model';
+import {
+    Language,
+    LanguageCreate,
+    LanguageUpdate,
+    LanguageFilters,
+} from '../models/language.model';
 import { buildHttpParams } from '../utils/http-utils';
 
-@Injectable({
-    providedIn: 'root',
-})
+@Injectable({ providedIn: 'root' })
 export class LanguageService {
     private http = inject(HttpClient);
-    private apiUrl = `${environment.apiUrl}/languages`;
+    private baseUrl = `${environment.apiUrl}/languages`;
 
     findAll(params: LanguageFilters): Observable<ApiResponse<Pagination<Language>>> {
-        return this.http.get<ApiResponse<Pagination<Language>>>(this.apiUrl, {
+        return this.http.get<ApiResponse<Pagination<Language>>>(this.baseUrl, {
             params: buildHttpParams(params),
         });
+    }
+
+    findOne(uuid: string): Observable<ApiResponse<Language>> {
+        return this.http.get<ApiResponse<Language>>(`${this.baseUrl}/${uuid}`);
+    }
+
+    create(dto: LanguageCreate): Observable<ApiResponse<Language>> {
+        return this.http.post<ApiResponse<Language>>(this.baseUrl, dto);
+    }
+
+    update(uuid: string, dto: LanguageUpdate): Observable<ApiResponse<Language>> {
+        return this.http.patch<ApiResponse<Language>>(
+            `${this.baseUrl}/${uuid}`,
+            dto
+        );
+    }
+
+    delete(uuid: string): Observable<ApiResponse<void>> {
+        return this.http.delete<ApiResponse<void>>(`${this.baseUrl}/${uuid}`);
     }
 }
