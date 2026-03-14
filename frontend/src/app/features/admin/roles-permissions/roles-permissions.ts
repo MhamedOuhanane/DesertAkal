9445664499ad6605f5f0fad77b76ccd06363a7fe
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2026-03-14 01:45:04 +0000

    FRD-377 #done: Configurer les routes et la navigation

---
+++ frontend/src/app/features/admin/roles-permissions/roles-permissions.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-roles-permissions',
+  imports: [],
+  templateUrl: './roles-permissions.html',
+  styles: ``,
+})
+export class RolesPermissions {
+
+}
