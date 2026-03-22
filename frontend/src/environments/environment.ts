c7e6b8931a7e33c07779313f592b5f4732a2dc06
M'hamed Ouhanane <mhmdeouhnan60@gmail.com>
2026-03-23 00:05:39 +0100

    Ajouter DockerFile avec nginx et cree image docker du front end

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:8080/api',
-    backUrl: 'http://localhost:8080',
+    apiUrl: 'https://desertakal.app/api',
+    backUrl: 'https://desertakal.app',
     secureCookie: true,
     touristRole: '1a9c496f-8c0d-492b-adce-1927e43c88d7',
 };
